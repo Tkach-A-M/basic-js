c7c8d6f9a93e881868ff95138c9b0e4f4c04ec3e
Alexandr Tkach <can4o1992@gmail.com>
2020-10-09 01:47:54 +0300

    feat: dream team pass all tests

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    // console.log(Array.isArray(members));
+    if(Array.isArray(members)){
+        let stringsArray = members.filter(member => typeof member === 'string');
+        console.log(stringsArray);
+        for(let i = 0; i < stringsArray.length; i++){stringsArray[i] = stringsArray[i].trim();}
+        console.log(stringsArray.sort());
+        let dreamTeamName = [];
+        stringsArray.forEach(item => dreamTeamName.push(item[0].toUpperCase()));
+        dreamTeamName.sort();
+        return dreamTeamName.join("");
+    }
+    else{
+        return false;
+    }
+
+
+        // if(Array.isArray(members)){
+        //     let r = members.filter(member => typeof member === 'string').sort();
+        //     console.log(r);
+        //     r.forEach(element => element.trim());
+        //     console.log(r);
+        //     let t = [];
+        //     r.forEach(item => t.push(item[0].toUpperCase()));
+        //     return t.join("");
+        // }
+        // else{
+        //     return false;
+        // }
+
 };
