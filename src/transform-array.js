683b742a3f623bb28411434c267f92f1a5d7a889
Alexandr Tkach <can4o1992@gmail.com>
2020-10-09 01:51:50 +0300

    feat:transform array pass basic tests

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let action = String(arr.filter(element => typeof element == 'string'));
+  // console.log(action);
+  // console.log(arr.indexOf(action));
+
+  if(arr.indexOf(action) == 0){
+    arr.splice(0, 1);
+  }
+  else if(arr.indexOf(action) == arr.length -1){
+    arr.splice(arr.indexOf(action -1, 1));
+  }
+  else{
+    switch (action) {
+      case '--discard-next':
+        arr.splice(arr.indexOf(action), 2);
+        break;
+
+      case '--discard-prev':
+        arr.splice(arr.indexOf(action)-1, 2);
+        break;
+
+      case '--double-next':
+        arr.splice(arr.indexOf(action), 1, arr.indexOf(action)+1);
+        break;
+    
+      case '--double-prev':
+        arr.splice(arr.indexOf(action), 1, arr.indexOf(action));
+        break;
+    
+      default:
+        break;
+    }
+  }
+  return arr;
 };
